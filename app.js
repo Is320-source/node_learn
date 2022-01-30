f6e2689305f021ea32658661c6e60780dc85b35d
Isidro Manuel <isidromanuel39@gmail.com>
2022-01-30 21:07:22 +0000

    studing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http')
 
-http.createServer( (request, response) => {
-    response.write("Ola Mundo")
-    response.end()
-}).listen(8081, () => { console.log("Server is Runnig! ")})
+http.createServer((request, response) => {
+    response.end("TEST")
+}).listen( 8081, () => {
+    console.log("Server is running rigth now")
+})
